59d3040657c71f886a8d2b07a6144082a27ae530
Abhimanyu Singh <abhimanyurollno2@gmail.com>
2022-08-19 11:57:48 +0530

    This version has the added implementation of adding new node and then saving all the notes to an array to display together, and also added the delete functoinality, press the delete button and the note is removed from the array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addItem(note) {
+    setNotes((prevValue) => {
+      return [...prevValue, note];
+    });
+  }
+
+  function deleteItem(id) {
+    setNotes((prevNote) => {
+      return prevNote.filter((items, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      {notes.map( note => 
-        <Note
-            key={note.key}
-            title={note.title}
-            content={note.content}
-        />
-      )}
+      <CreateArea addNote={addItem} />
+      {notes.map((slip, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={slip.title}
+            content={slip.content}
+            onDelete={deleteItem}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ root.render(
   </React.StrictMode>
 );
 
-
-
+//Challenge 1
 //1. Create a new React app.
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
@@ -24,5 +23,20 @@ root.render(
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 
-//Challenge. Render all the notes inside notes.js as a seperate Note
+//Challenge 2. Render all the notes inside notes.js as a seperate Note
 //component.
+
+//CHALLENGE 3:
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
